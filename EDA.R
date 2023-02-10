dd3682e58643dc793e5e0646e88e76ac28e1cc34
Julia Ma <maj@cua.edu>
2023-02-10 00:20:08 -0500

    Update EDA.R
    starting filtering and first data viz

--- EDA.R
+++ EDA.R
@@ -1,9 +1,11 @@
 # install required packages; only needs to be done once
 #install.packages("tidyverse")
 #install.packages("nflfastR")
+#install.packages("nflreadr")
 #install.packages("ggimage")
 #install.packages("gt")
 #install.packages("dplyr")
+#install.packages("nflplotR")
 
 # import libraries; must be done for each session
 library(tidyverse)
@@ -11,22 +13,38 @@ library(nflfastR)
 library(ggimage)
 library(gt)
 library(dplyr)
+library(nflplotR)
+library(ggplot2)
 
-# get 2021-2022 data
-pbp <- load_pbp(2021:2022)
+# get 2016-2021 data
+pbp <- load_pbp(2016:2021)
 pbp %>% head()
 
 # browse available columns
 names(pbp)
 
-# get only Giants offense plays; no kickoffs or punts
-nyg_pbp <- pbp %>% filter(posteam == "NYG") %>% select(defteam, down, ydstogo, play_type, play_type_nfl, result, yards_gained)
-nyg_pbp <- nyg_pbp %>% filter(play_type!="kickoff" & play_type!="punt")
+# get total plays that resulted in TD
+all_tds <- pbp %>% filter(touchdown==1) %>% select(posteam,season,td_team,touchdown)
+# filter out defensive TDs
+off_td <- all_tds %>% filter(td_team==posteam)
+# count by team
+tot_td <- count(off_td,posteam)
+tot_td
 
-table(nyg_pbp['play_type'])
-# extra point; field goal; no play; pass; qb kneel; qb spike; run
-# perhaps we only care about pass and run? 
-table(nyg_pbp['play_type_nfl'])
-# nfl types are field goal; pass; pat2; penalty; rush; sack; xp kick
-# do we treat sack/PAT2 differently?
+######fourth down stuff######
+go_for_it<- pbp %>% filter(fourth_down_converted==1 | fourth_down_failed==1) %>% select(posteam, fourth_down_converted)
+tot_fourth <- count(go_for_it,posteam)
+fourth_success <- go_for_it %>% filter(fourth_down_converted==1)
+tot_fourth['success'] <- count(fourth_success,posteam)[,2]
+tot_fourth['rate'] <- tot_fourth[,3]/tot_fourth[,2]
+tot_fourth
 
+combined <- tot_fourth %>% inner_join(tot_td, by="posteam")
+combined
+
+ggplot(combined, aes(x = n.x, y = n.y)) + 
+  nflplotR::geom_mean_lines(aes(v_var = n.x, h_var = n.y)) +
+  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
+  labs(x = "Fourth Down Attempts",
+       y = "Offensive TDs",
+       title = "Fourth Down Go-For-It vs. TDs")
