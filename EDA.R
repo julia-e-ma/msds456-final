b2b85f7b8f54593ad2172478d7c330d8142875c6
Julia Ma <maj@cua.edu>
2023-02-14 00:42:44 -0500

    add ML Libraries
    - linear regression
    - random forest
    - xgboost
    - TODO: neural network

--- EDA.R
+++ EDA.R
@@ -6,6 +6,9 @@
 #install.packages("gt")
 #install.packages("dplyr")
 #install.packages("nflplotR")
+#install.packages("randomForest")
+#install.packages("xgboost")
+#install.packages("neuralnet")
 
 # import libraries; must be done for each session
 library(tidyverse)
@@ -15,6 +18,9 @@ library(gt)
 library(dplyr)
 library(nflplotR)
 library(ggplot2)
+library(randomForest)
+library(xgboost)
+library(neuralnet)
 
 # get 2016-2021 data
 pbp <- load_pbp(2016:2021)
@@ -49,48 +55,39 @@ ggplot(combined, aes(x = n.x, y = n.y)) +
        y = "Offensive TDs",
        title = "Fourth Down Go-For-It vs. TDs")
 
-#####Success Rates vs # of touchdowns#####
-library(readr)
-all_NFL_Success_Rates <- read_csv("C:/Users/bshos/OneDrive/Desktop/MSDS 456/Final Project/all_NFL_Success_Rates.csv") #will need to change this obviously
-success_rates <- all_NFL_Success_Rates %>%
-                    mutate(total_tds = tot_td$n)
 
-#Total Success Rate
-ggplot(success_rates, aes(x = total_tds, y = total_successrate_all)) + 
-  nflplotR::geom_mean_lines(aes(v_var = total_tds, h_var = total_successrate_all)) +
-  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
-  labs(x = "Offensive TDs",
-       y = "Offensive Success Rate",
-       title = "Success Rate vs. TDs")
+#### linear regression ####
+relation <- lm(n.y ~ n.x + rate, combined)
+print(summary(relation))
 
-#Run Success Rate
-ggplot(success_rates, aes(x = total_tds, y = run_successrate_all)) + 
-  nflplotR::geom_mean_lines(aes(v_var = total_tds, h_var = run_successrate_all)) +
-  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
-  labs(x = "Offensive TDs",
-       y = "Run Success Rate",
-       title = "Run Success Rate vs. TDs")
+#### random forest ####
+set.seed(1234)
+rf.fit <- randomForest(n.y ~ ., data=combined, ntree=1000, importance=TRUE)
+rf.fit
 
-#Pass Success Rate
-ggplot(success_rates, aes(x = total_tds, y = pass_successrate_all)) + 
-  nflplotR::geom_mean_lines(aes(v_var = total_tds, h_var = pass_successrate_all)) +
-  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
-  labs(x = "Offensive TDs",
-       y = "Pass Success Rate",
-       title = "Pass Success Rate vs. TDs")
+# Get variable importance from the model fit
+ImpData <- as.data.frame(importance(rf.fit))
+ImpData$Var.Names <- row.names(ImpData)
 
-#1st Success Rate
-ggplot(success_rates, aes(x = total_tds, y = total_successrate_1st)) + 
-  nflplotR::geom_mean_lines(aes(v_var = total_tds, h_var = total_successrate_1st)) +
-  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
-  labs(x = "Offensive TDs",
-       y = "First Down Success Rate",
-       title = "First Down Success Rate vs. TDs")
+ggplot(ImpData, aes(x=Var.Names, y=`%IncMSE`)) +
+  geom_segment( aes(x=Var.Names, xend=Var.Names, y=0, yend=`%IncMSE`), color="skyblue") +
+  geom_point(aes(size = IncNodePurity), color="blue", alpha=0.6) +
+  theme_light() +
+  coord_flip() +
+  theme(
+    legend.position="bottom",
+    panel.grid.major.y = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks.y = element_blank()
+  )
 
-#3rd Success Rate
-ggplot(success_rates, aes(x = total_tds, y = total_3rd)) + 
-  nflplotR::geom_mean_lines(aes(v_var = total_tds, h_var = total_3rd)) +
-  nflplotR::geom_nfl_logos(aes(team_abbr = posteam), width = 0.065, alpha = 0.7) +
-  labs(x = "Offensive TDs",
-       y = "Third Down Conversion Rate",
-       title = "Third Down Conversion Rate vs. TDs")
+#### xgboost ####
+x = data.matrix(combined[,2:4])
+y = data.matrix(combined[,5])
+model_xgb <- xgboost(data = x, label = y, nrounds=10)
+imp <- xgb.importance(model = model_xgb)
+print(imp)
+xgb.plot.importance(importance_matrix = imp)
+
+#### neural network ####
+
